e68b22ef3fc68dbc30be6ba7775b3b1afa80abc0
Satrya Tangguh <satryatp@gmail.com>
2022-11-12 21:43:39 +0700

    Add navbar scroll effect

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
+// Navbar top scroll
+const navTop = document.querySelector('.navbar-top');
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 50) {
+        navTop.classList.add('active_nav');
+    } else {
+        navTop.classList.remove('active_nav');
+    }
+})
+
+// Navbar down scroll
+const navDown = document.querySelector('.navbar-down');
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 50) {
+        navDown.classList.add('active_nav');
+    } else {
+        navDown.classList.remove('active_nav');
+    }
+})
+
+
 // Arrow Bottom to Top
 const toTop = document.querySelector(".to-top");
-
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 80) {
         toTop.classList.add("active");
