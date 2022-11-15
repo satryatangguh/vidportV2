35dc425b7a4d58db58955a7311c0e1554182ac91
Satrya Tangguh <satryatp@gmail.com>
2022-11-15 22:00:52 +0700

    Add alert for contact us

--- js/main.js
+++ js/main.js
@@ -49,3 +49,34 @@ function readMore(btn){
     post.querySelector(".more").classList.toggle("hide");
     btn.textContent == "Read More" ? btn.textContent = "Read Less" : btn.textContent = "Read More";
 }
+
+// Contact Us Alert
+function message(){
+    var Name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var subject = document.getElementById('subject');
+    var msg = document.getElementById('msg');
+    const success = document.getElementById('success');
+    const danger = document.getElementById('danger');
+
+    if(Name.value === '' || email.value === '' || subject.value === '' || msg.value === ''){
+        danger.style.display = 'block';
+    }
+    else{
+        setTimeout(() => {
+            Name.value = '';
+            email.value = '';
+            subject.value = '';
+            msg.value = '';
+        }, 2000);
+
+        success.style.display = 'block';
+    }
+
+
+    setTimeout(() => {
+        danger.style.display = 'none';
+        success.style.display = 'none';
+    }, 3000);
+
+}
